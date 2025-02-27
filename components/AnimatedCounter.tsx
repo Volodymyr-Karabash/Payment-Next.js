cb135426cc18530ed85158066b3ba1c9300d1276
Volodymyr-Karabash <voca480@gmail.com>
2025-02-27 14:45:44 +0200

    AnimatedCounter.tsx Update

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -4,7 +4,7 @@ import CountUp from 'react-countup';
 
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <div className="w-full">
+    <div className="w-full text-whtie font-weight-bold">
       <CountUp 
         decimals={2}
         decimal=","
